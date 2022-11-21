895cdf21ea8b841813c1f5a2a3ff8871edc34b0e
jiahao70000 <77180495+jiahao70000@users.noreply.github.com>
2022-11-20 19:44:47 -0800

    Add files via upload

---
+++ fancifymytext.js
@@ -0,0 +1,22 @@
+function Bigger() {
+    document.getElementById("txtArea").style.fontSize = "24pt";
+}
+
+function fancyShmancy() {
+    document.getElementById("txtArea").style.fontWeight = "bold";
+    document.getElementById("txtArea").style.color = 'blue';
+    document.getElementById("txtArea").style.textDecoration = "underline";
+
+}
+
+function boringBetty() {
+    document.getElementById("txtArea").style.fontWeight = "normal";
+    document.getElementById("txtArea").style.color = "black";
+    document.getElementById("txtArea").style.textDecoration = "none";
+}
+
+function Moo() {
+    var str = document.getElementById("txtArea");
+    var parts = str.value.split(".");
+    str.value = parts.join("-Moo");
+}
